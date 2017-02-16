1449c62fe4cfe6035a867afdd466262d6933d17b
omer.altinuc <omer.alitnuc@hotmail.com>
2017-02-16 14:42:28 +0200

    database configurasyonu yapıldı

--- secimyolu/secimyolu/Models/SECIMYOLUEntities.Designer.cs
+++ secimyolu/secimyolu/Models/SECIMYOLUEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\KASTANYET_PC\Source\Repos\SecimYolu\SecimYolu\secimyolu\secimyolu\Models\SECIMYOLUEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\SecimYolu\secimyolu\secimyolu\Models\SECIMYOLUEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
